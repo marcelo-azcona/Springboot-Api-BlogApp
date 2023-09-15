4225e10abf5210f7e9ab91d5266ed48dcef4b460
marcelo-azcona <marceloazcona86@gmail.com>
2023-09-15 19:17:55 -0300

    Add PostController with createPost method

--- src/main/java/com/springboot/blogapp/controller/PostController.java
+++ src/main/java/com/springboot/blogapp/controller/PostController.java
@@ -1,4 +1,31 @@
 package com.springboot.blogapp.controller;
 
+import com.springboot.blogapp.dto.PostDto;
+import com.springboot.blogapp.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // create blog post endpoint
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto),
+                HttpStatus.CREATED
+        );
+    }
 }
