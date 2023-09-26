7bea8913cc7876528f3bc7dab6fef3a517d00f57
marcelo-azcona <marceloazcona86@gmail.com>
2023-09-26 20:40:10 -0300

    Created CommentRepository

---
+++ src/main/java/com/springboot/blogapp/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.blogapp.repository;
+
+import com.springboot.blogapp.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
