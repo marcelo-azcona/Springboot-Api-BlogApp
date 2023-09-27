ae87cd12f2d87007b342cb3fe886e11257ebc795
marcelo-azcona <marceloazcona86@gmail.com>
2023-09-27 19:45:42 -0300

    Added createComment method and fixed some indentation errors

--- src/main/java/com/springboot/blogapp/service/CommentServiceImpl.java
+++ src/main/java/com/springboot/blogapp/service/CommentServiceImpl.java
@@ -2,7 +2,10 @@ package com.springboot.blogapp.service;
 
 import com.springboot.blogapp.dto.CommentDto;
 import com.springboot.blogapp.entity.Comment;
+import com.springboot.blogapp.entity.Post;
+import com.springboot.blogapp.exception.ResourceNotFoundException;
 import com.springboot.blogapp.repository.CommentRepository;
+import com.springboot.blogapp.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,18 +14,36 @@ public class CommentServiceImpl implements CommentService {
 
 
     private CommentRepository commentRepository;
+    private PostRepository postRepository;
 
     @Autowired
-    public CommentServiceImpl(CommentRepository commentRepository) {
+    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository) {
         this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
     }
 
     @Override
     public CommentDto createComment(long postId, CommentDto commentDto) {
-        return null;
+
+        Comment comment = dtoToEntity(commentDto);
+
+        // Retrieve post by id
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new ResourceNotFoundException("Post", "id", postId));
+
+        // Set post to comment entity
+        comment.setPost(post);
+
+        // Save comment entity to DB
+        Comment savedComment = commentRepository.save(comment);
+
+        CommentDto savedCommentDto = entityToDto(savedComment);
+        
+        return savedCommentDto;
     }
 
     private CommentDto entityToDto(Comment comment) {
+
         CommentDto commentDto = new CommentDto();
         commentDto.setId(comment.getId());
         commentDto.setBody(comment.getBody());
@@ -33,6 +54,7 @@ public class CommentServiceImpl implements CommentService {
     }
 
     private Comment dtoToEntity(CommentDto commentDto) {
+
         Comment comment = new Comment();
         comment.setId(commentDto.getId());
         comment.setBody(commentDto.getBody());
