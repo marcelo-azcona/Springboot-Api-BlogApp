41af732bfb791f2df4f13d407ea3b8fa66694611
marcelo-azcona <marceloazcona86@gmail.com>
2023-09-27 19:07:53 -0300

    Implement methods for change entitys to DTO and viceversa

--- src/main/java/com/springboot/blogapp/service/CommentServiceImpl.java
+++ src/main/java/com/springboot/blogapp/service/CommentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.springboot.blogapp.service;
 
 import com.springboot.blogapp.dto.CommentDto;
+import com.springboot.blogapp.entity.Comment;
 import com.springboot.blogapp.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,25 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public CommentDto createComment(long postId, CommentDto commentDto) {
         return null;
+    }
+
+    private CommentDto entityToDto(Comment comment) {
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setBody(comment.getBody());
+        commentDto.setName(comment.getName());
+        commentDto.setEmail(comment.getEmail());
+
+        return commentDto;
+    }
+
+    private Comment dtoToEntity(CommentDto commentDto) {
+        Comment comment = new Comment();
+        comment.setId(commentDto.getId());
+        comment.setBody(commentDto.getBody());
+        comment.setName(comment.getName());
+        comment.setEmail(comment.getEmail());
+
+        return comment;
     }
 }
