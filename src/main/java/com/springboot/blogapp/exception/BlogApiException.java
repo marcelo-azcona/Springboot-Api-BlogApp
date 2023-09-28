5d447e9c2757366164c2e500e91790dcd78034f7
marcelo-azcona <marceloazcona86@gmail.com>
2023-09-28 20:47:45 -0300

    Exception to validate request parameters and business logic

---
+++ src/main/java/com/springboot/blogapp/exception/BlogApiException.java
@@ -0,0 +1,24 @@
+package com.springboot.blogapp.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BlogApiException extends RuntimeException {
+
+    private final HttpStatus status;
+    private String message;
+
+    public BlogApiException(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public BlogApiException(String message, HttpStatus status, String message1) {
+        super(message);
+        this.status = status;
+        this.message = message1;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
