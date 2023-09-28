fe0f5db4968c701149a0dd92cf4b304793b75126
marcelo-azcona <marceloazcona86@gmail.com>
2023-09-28 19:09:18 -0300

    Added getCommentsByPostId method

--- src/main/java/com/springboot/blogapp/service/CommentService.java
+++ src/main/java/com/springboot/blogapp/service/CommentService.java
@@ -1,8 +1,13 @@
 package com.springboot.blogapp.service;
 
 import com.springboot.blogapp.dto.CommentDto;
+import com.springboot.blogapp.entity.Comment;
+
+import java.util.List;
 
 public interface CommentService {
 
     CommentDto createComment(long postId, CommentDto commentDto);
+
+    List<Comment> getCommentsByPostId(long postId);
 }
