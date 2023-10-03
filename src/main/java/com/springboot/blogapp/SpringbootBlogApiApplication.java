3e3cf8f2f7ceecec40ca5f589f0ceebe735e2861
marcelo-azcona <marceloazcona86@gmail.com>
2023-10-03 19:07:45 -0300

    Created ModelMapper spring bean

--- src/main/java/com/springboot/blogapp/SpringbootBlogApiApplication.java
+++ src/main/java/com/springboot/blogapp/SpringbootBlogApiApplication.java
@@ -1,13 +1,20 @@
 package com.springboot.blogapp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootBlogApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBlogApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBlogApiApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 }
