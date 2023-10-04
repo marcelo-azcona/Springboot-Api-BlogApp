5a6a0753d380673d7c96d076f7c951f41d4a5f2f
marcelo-azcona <marceloazcona86@gmail.com>
2023-10-03 21:25:40 -0300

    Added a global exception handler

--- src/main/java/com/springboot/blogapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/blogapp/exception/GlobalExceptionHandler.java
@@ -35,4 +35,17 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
+
+    // Global Exception
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(ResourceNotFoundException exception,
+                                                              WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false)
+        );
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
