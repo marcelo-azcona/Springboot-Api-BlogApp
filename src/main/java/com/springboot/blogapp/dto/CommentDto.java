ef37844d4cb3fae32e50229cdf07778c83f48fdc
marcelo-azcona <marceloazcona86@gmail.com>
2023-10-05 20:48:35 -0300

    Added validation constrains to comments

--- src/main/java/com/springboot/blogapp/dto/CommentDto.java
+++ src/main/java/com/springboot/blogapp/dto/CommentDto.java
@@ -1,12 +1,23 @@
 package com.springboot.blogapp.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
-    
+
     private Long id;
+
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
+
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
     private String email;
+
+    @NotEmpty
+    @Size(min = 10, message = "The comment must be at least 10 characters")
     private String body;
 }
