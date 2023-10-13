c9536c7b5cba23f5f405dc4f6feb6f2be7abf3b4
marcelo-azcona <marceloazcona86@gmail.com>
2023-10-13 20:12:31 -0300

    Created endpoint to add categories to the DB

---
+++ src/main/java/com/springboot/blogapp/controller/CategoryController.java
@@ -0,0 +1,33 @@
+package com.springboot.blogapp.controller;
+
+import com.springboot.blogapp.dto.CategoryDto;
+import com.springboot.blogapp.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/categories")
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public ResponseEntity<CategoryDto> addCategory(@RequestBody CategoryDto categoryDto) {
+
+        CategoryDto addedCategory = categoryService.addCategory(categoryDto);
+
+        return new ResponseEntity<>(addedCategory, HttpStatus.CREATED);
+    }
+}
