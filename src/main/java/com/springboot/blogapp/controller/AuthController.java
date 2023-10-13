77890838aa8ea631ff0487a8123bc156afe76a6a
marcelo-azcona <marceloazcona86@gmail.com>
2023-10-13 14:57:12 -0300

    Controller to handle login request

---
+++ src/main/java/com/springboot/blogapp/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.springboot.blogapp.controller;
+
+import com.springboot.blogapp.dto.LoginDto;
+import com.springboot.blogapp.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/auth")
+public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        String response = authService.login(loginDto);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+}
