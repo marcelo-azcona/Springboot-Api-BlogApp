111fdf6533f59bd0b72a2818b298c6f4333a6c26
marcelo-azcona <marceloazcona86@gmail.com>
2023-10-13 15:23:03 -0300

    RegisterDto class

---
+++ src/main/java/com/springboot/blogapp/dto/RegisterDto.java
@@ -0,0 +1,17 @@
+package com.springboot.blogapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterDto {
+
+    private String username;
+    private String password;
+    private String email;
+}
