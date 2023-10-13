edd038e2b837f3ed6f44c1799b0a48f48058e5ee
marcelo-azcona <marceloazcona86@gmail.com>
2023-10-13 19:17:42 -0300

    Defined CategoryService with a method to add categories

---
+++ src/main/java/com/springboot/blogapp/service/CategoryService.java
@@ -0,0 +1,8 @@
+package com.springboot.blogapp.service;
+
+import com.springboot.blogapp.dto.CategoryDto;
+
+public interface CategoryService {
+    
+    CategoryDto addCategory(CategoryDto categoryDto);
+}
