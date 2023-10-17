ac6c0976b6dabf7a93007aa898803f73661bf7c1
marcelo-azcona <marceloazcona86@gmail.com>
2023-10-17 09:39:43 -0300

    Implemented the method to get a category

--- src/main/java/com/springboot/blogapp/service/CategoryServiceImpl.java
+++ src/main/java/com/springboot/blogapp/service/CategoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.springboot.blogapp.service;
 
 import com.springboot.blogapp.dto.CategoryDto;
 import com.springboot.blogapp.entity.Category;
+import com.springboot.blogapp.exception.ResourceNotFoundException;
 import com.springboot.blogapp.repository.CategoryRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,13 @@ public class CategoryServiceImpl implements CategoryService {
         Category savedCategory = categoryRepository.save(category);
 
         return modelMapper.map(savedCategory, CategoryDto.class);
+    }
+
+    @Override
+    public CategoryDto getCategory(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId).orElseThrow(
+                () -> new ResourceNotFoundException("Category", "id", categoryId));
+
+        return modelMapper.map(category, CategoryDto.class);
     }
 }
