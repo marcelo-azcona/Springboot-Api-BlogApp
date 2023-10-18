5b84953baef7189c3877eff55204db38699f2d4d
marcelo-azcona <marceloazcona86@gmail.com>
2023-10-18 09:48:45 -0300

    Created a query method to retrieve a list of blog post

--- src/main/java/com/springboot/blogapp/repository/PostRepository.java
+++ src/main/java/com/springboot/blogapp/repository/PostRepository.java
@@ -4,7 +4,10 @@ import com.springboot.blogapp.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    List<Post> findByCategoryId(Long categoryId);
 }
