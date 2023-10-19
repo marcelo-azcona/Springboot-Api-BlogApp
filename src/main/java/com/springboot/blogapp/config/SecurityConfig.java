d3fcf078a237708deea18c9ea01b753c31b85747
marcelo-azcona <marceloazcona86@gmail.com>
2023-10-19 20:36:20 -0300

    Added public access to swagger ui

--- src/main/java/com/springboot/blogapp/config/SecurityConfig.java
+++ src/main/java/com/springboot/blogapp/config/SecurityConfig.java
@@ -45,6 +45,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize ->
                         authorize.requestMatchers(HttpMethod.GET, "/v1/**").permitAll()
                                 .requestMatchers("/v1/auth/**").permitAll()
+                                .requestMatchers("/swagger-ui/**").permitAll()
+                                .requestMatchers("/v3/api-docs").permitAll()
                                 .anyRequest().authenticated()
                 ).httpBasic(Customizer.withDefaults());
 
